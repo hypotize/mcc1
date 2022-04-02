a201efab4feb6c021cd24c8297922127b88d7dc4
Michael Cashen <michael@hypotize.com>
2022-04-03 08:29:52 +0900

    Clean up and add comments

--- 0403_lesson/step3.py
+++ 0403_lesson/step3.py
@@ -1,3 +1,10 @@
+#　Ping Pong game Step 3  - Make ball bouce off paddle 
+# Michael Cashen 
+# Builds on Tom's Pong 
+# https://github.com/cprn/pong/blob/master/pong.py
+#
+# Released under the GNU General Public License
+
 import sys
 import math
 import os
@@ -9,179 +16,176 @@ from pygame.locals import *
 def load_png(name):
     try:
         image = pygame.image.load(name)
-        image = image.convert_alpha()
     except pygame.error as message:
         print('Cannot load image:', fullname)
         raise SystemExit(message)
     return image, image.get_rect()
 
+
+# ボール　クラスの定義
 class Ball(pygame.sprite.Sprite):
 
+
+    #初期化
     def __init__(self, startpos, vector):
         pygame.sprite.Sprite.__init__(self)
 
         self.image, self.rect = load_png('ball.png')
+
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
+
         self.vector = vector
-        self.hit = 0
-        self.rect.centerx = startpos[0]
-        self.rect.centery = startpos[1]
+        self.rect.center = startpos
 
+    #更新
     def update(self):
-        newpos = self.calcnewpos(self.rect,self.vector)
+        
+        #　角度とスピードからx軸と y軸の移動距離を計算
+        # https://hakuhin.jp/as/math.html 
+        (angle,speed) = self.vector
+        x_change = speed * math.cos(angle)
+        y_change = speed * math.sin(angle)
+        newpos = self.rect.move(x_change,y_change)
         self.rect = newpos
-        (angle,z) = self.vector
 
+        # ボールの一部でもエリアの外行った場合の処理
         if not self.area.contains(newpos):
-            tl = not self.area.collidepoint(newpos.topleft)
-            tr = not self.area.collidepoint(newpos.topright)
-            bl = not self.area.collidepoint(newpos.bottomleft)
-            br = not self.area.collidepoint(newpos.bottomright)
-            if tr and tl or (br and bl):
-                angle = -angle
-            if tl and bl:
-                angle = math.pi - angle
-            if tr and br:
-                angle = math.pi - angle
 
+            #左上が外ならTrue
+            tl = not self.area.collidepoint(newpos.topleft)
+
+            #右上が外ならTrue
+            tr = not self.area.collidepoint(newpos.topright)
+
+            #左下が外ならTrue
+            bl = not self.area.collidepoint(newpos.bottomleft)
+
+            #右下が外ならTrue
+            br = not self.area.collidepoint(newpos.bottomright)
+
+            #上の辺
+            if tr and tl:
+                print("hit top wall")
+                new_angle = -angle 
+
+            #下の辺
+            if br and bl:
+                print("hit bottom wall")
+                new_angle = -angle 
+
+            #左の辺
+            if tl and bl:
+                print("hit left wall")
+                new_angle = math.pi - angle
+
+            #右の辺
+            if tr and br:
+                print("hit right wall")
+                new_angle = math.pi - angle
+                
+            if new_angle <  0: 
+               new_angle  = new_angle + math.pi * 2  
+
+
+            print(angle, " => ", new_angle)
+            self.vector = (new_angle,speed)
+            
+        # ボールがパドルに当たった場合の処理
         if self.rect.colliderect(paddle.rect) == 1:
-            print('hit paddle')
-            print(angle)
-            # 角度はラジアン
-            # https://univ-juken.com/kodoho
-            # 0 は右方向 2 π で一周
             minangle =   math.pi *1.25
             maxangle =  math.pi *1.75
-            angle = random.uniform(minangle, maxangle)
-        self.vector = (angle,z)
+            new_angle = random.uniform(minangle, maxangle)
+            
+            print('hit paddle')
+            print(angle, " => ", new_angle)
+            self.vector = (new_angle,speed)
 
-    def calcnewpos(self,rect,vector):
-        (angle,z) = vector
-        (dx,dy) = (z*math.cos(angle),z*math.sin(angle))
-        return rect.move(dx,dy)
 
+# パドル　クラスの定義
 class Paddle(pygame.sprite.Sprite):
 
+    #初期化
     def __init__(self, startpos):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_png('paddle.png')
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
-        self.rect.centerx = startpos[0]
-        self.rect.centery = startpos[1]
+        self.rect.center = startpos
         
     def update(self):
         mousepos = pygame.mouse.get_pos()
         self.rect.midtop = (mousepos[0], 300)    
     
-class Bat(pygame.sprite.Sprite):
-    """Movable tennis 'bat' with which one hits the ball
-    Returns: bat object
-    Functions: reinit, update, moveup, movedown
-    Attributes: which, speed"""
 
-    def __init__(self, side):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_png('bat.png')
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.side = side
-        self.speed = 10
-        self.state = "still"
-        self.reinit()
-
-    def reinit(self):
-        self.state = "still"
-        self.movepos = [0,0]
-        if self.side == "left":
-            self.rect.midleft = self.area.midleft
-        elif self.side == "right":
-            self.rect.midright = self.area.midright
-
-    def update(self):
-        newpos = self.rect.move(self.movepos)
-        if self.area.contains(newpos):
-            self.rect = newpos
-        pygame.event.pump()
-    def moveup(self):
-        self.movepos[1] = self.movepos[1] - (self.speed)
-        self.state = "moveup"
-
-    def movedown(self):
-        self.movepos[1] = self.movepos[1] + (self.speed)
-        self.state = "movedown"
 
 def main():
+
     # 画面の初期化
     pygame.init()
     screen = pygame.display.set_mode((480, 360))
-    pygame.display.set_caption('ピンポンゲームステップ1')
+    pygame.display.set_caption('ピンポンゲーム ステップ3')
 
     # 背景
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((255, 255,255))
 
-    # Initialise players
+    #ボールの処理からパドル呼べるように、グローバル化しておく
     global paddle
 
+
     # ボール初期化
-    #  (初期x座標,初期y座標), (初期角度ラジアン, 速度)
-    ball = Ball((240,100),(-0.6,6))
+    # (初期x座標,初期y座標), (初期角度ラジアン, 速度)
+    ball = Ball((240,100),(math.pi * 1.75,6))
 
     # パドル初期化
     paddle = Paddle((10,300))
-    
+
     # 描く
-#    playersprites = pygame.sprite.RenderPlain((player1, player2))
     ballsprite = pygame.sprite.RenderPlain(ball)
     paddlesprite = pygame.sprite.RenderPlain(paddle)
 
     # 画面に出す
     screen.blit(background, (0, 0))
-    #pygame.display.flip()
 
-    # Initialise clock
+    # 動きを管理するClockを初期化
     clock = pygame.time.Clock()
 
-    # Event loop
+    # ずっと処理
     while 1:
-        # Make sure game doesn't run at more than 60 frames per second
+
+        # 一秒に最大60回処理
         clock.tick(60)
 
+        #イベント処理
         for event in pygame.event.get():
+
+            #画面を閉じるボタン
             if event.type == QUIT:
                 return
-            if event.type == KEYDOWN:
-                if event.key == K_q:
-                    return
- #                if event.key == K_a:
- #                   player1.moveup()
- #               if event.key == K_z:
- #                   player1.movedown()
- #               if event.key == K_UP:
- #                   player2.moveup()
- #               if event.key == K_DOWN:
- #                   player2.movedown()
- #           elif event.type == KEYUP:
- #               if event.key == K_a or event.key == K_z:
- #                   player1.movepos = [0,0]
- #                   player1.state = "still"
- #               if event.key == K_UP or event.key == K_DOWN:
- #                   player2.movepos = [0,0]
- #                   player2.state = "still"
 
+            #キーが押されたとき
+            if event.type == KEYDOWN:
+                if event.key == K_q or event.key == K_ESCAPE:
+                    return
+
+        #ボールとパドルがあった場所に背景をもう一回描く
         screen.blit(background, ball.rect, ball.rect)
         screen.blit(background, paddle.rect, paddle.rect)
-        #screen.blit(background, player2.rect, player2.rect)
+
+
+        #ボールとパドルのアップデート処理(位置計算)
         ballsprite.update()
         paddlesprite.update()
-        #playersprites.update()
+
+
+        #ボールを描く
         ballsprite.draw(screen)
-        
         paddlesprite.draw(screen)
-        #playersprites.draw(screen)
+
+
+        #表示更新
         pygame.display.flip()
 
 
--- 0403_lesson/step5.py
+++ 0403_lesson/step5.py
@@ -1,3 +1,10 @@
+#　Ping Pong game Step 5  - Add score and speed
+# Michael Cashen 
+# Builds on Tom's Pong 
+# https://github.com/cprn/pong/blob/master/pong.py
+#
+# Released under the GNU General Public License
+
 import sys
 import math
 import os
@@ -9,142 +16,192 @@ from pygame.locals import *
 def load_png(name):
     try:
         image = pygame.image.load(name)
-        image = image.convert_alpha()
     except pygame.error as message:
         print('Cannot load image:', fullname)
         raise SystemExit(message)
     return image, image.get_rect()
 
+
+# ボール　クラスの定義
 class Ball(pygame.sprite.Sprite):
 
+
+    #初期化
     def __init__(self, startpos, vector):
         pygame.sprite.Sprite.__init__(self)
 
         self.image, self.rect = load_png('ball.png')
+
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
+
         self.vector = vector
-        self.hit = 0
-        self.rect.centerx = startpos[0]
-        self.rect.centery = startpos[1]
+        self.rect.center = startpos
 
+    #更新
     def update(self):
-        newpos = self.calcnewpos(self.rect,self.vector)
+        global speed
+        #　角度とスピードからx軸と y軸の移動距離を計算
+        # https://hakuhin.jp/as/math.html 
+        angle = self.vector[0]
+        x_change = speed * math.cos(angle)
+        y_change = speed * math.sin(angle)
+        newpos = self.rect.move(x_change,y_change)
         self.rect = newpos
-        (angle,z) = self.vector
 
+        # ボールの一部でもエリアの外行った場合の処理
         if not self.area.contains(newpos):
-            tl = not self.area.collidepoint(newpos.topleft)
-            tr = not self.area.collidepoint(newpos.topright)
-            bl = not self.area.collidepoint(newpos.bottomleft)
-            br = not self.area.collidepoint(newpos.bottomright)
-            if tr and tl or (br and bl):
-                angle = -angle
-            if tl and bl:
-                angle = math.pi - angle
-            if tr and br:
-                angle = math.pi - angle
 
+            #左上が外ならTrue
+            tl = not self.area.collidepoint(newpos.topleft)
+
+            #右上が外ならTrue
+            tr = not self.area.collidepoint(newpos.topright)
+
+            #左下が外ならTrue
+            bl = not self.area.collidepoint(newpos.bottomleft)
+
+            #右下が外ならTrue
+            br = not self.area.collidepoint(newpos.bottomright)
+
+            #上の辺
+            if tr and tl:
+                print("hit top wall")
+                new_angle = -angle 
+
+            #下の辺
+            if br and bl:
+                print("hit bottom wall")
+                new_angle = -angle 
+
+            #左の辺
+            if tl and bl:
+                print("hit left wall")
+                new_angle = math.pi - angle
+
+            #右の辺
+            if tr and br:
+                print("hit right wall")
+                new_angle = math.pi - angle
+                
+            if new_angle <  0: 
+               new_angle  = new_angle + math.pi * 2  
+
+
+            print(angle, " => ", new_angle)
+            self.vector = (new_angle,speed)
+            
+        # ボールがパドルに当たった場合の処理
         if self.rect.colliderect(paddle.rect) == 1:
-            print('hit paddle')
-            print(angle)
-            # 角度はラジアン
-            # https://univ-juken.com/kodoho
-            # 0 は右方向 2 π で一周
             minangle =   math.pi *1.25
             maxangle =  math.pi *1.75
-            angle = random.uniform(minangle, maxangle)
+            new_angle = random.uniform(minangle, maxangle)
+            
+            print('hit paddle')
+            print(angle, " => ", new_angle)
+            self.vector = (new_angle,speed)
+            
             global score
             score += 1
 
+
+        # ボールが線に当たった場合の処理
         if self.rect.colliderect(line.rect) == 1:
             print('hit line')
-            z = 0
-            global speed
             speed = 0
-        self.vector = (angle,z)
+            self.vector = (angle,speed)
 
-    def calcnewpos(self,rect,vector):
-        (angle,z) = vector
-        (dx,dy) = (speed * math.cos(angle),speed * math.sin(angle))
-        return rect.move(dx,dy)
 
+# パドル　クラスの定義
 class Paddle(pygame.sprite.Sprite):
 
+    #初期化
     def __init__(self, startpos):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_png('paddle.png')
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
-        self.rect.centerx = startpos[0]
-        self.rect.centery = startpos[1]
+        self.rect.center = startpos
         
     def update(self):
         mousepos = pygame.mouse.get_pos()
         self.rect.midtop = (mousepos[0], 300)    
-    
+
+# 線　クラスの定義
 class Line(pygame.sprite.Sprite):
 
+    #初期化
     def __init__(self, startpos):
         pygame.sprite.Sprite.__init__(self)
         self.image, self.rect = load_png('line.png')
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
-        self.rect.centerx = startpos[0]
-        self.rect.centery = startpos[1]
+        self.rect.center = startpos
+
 
 def main():
+
     # 画面の初期化
     pygame.init()
     screen = pygame.display.set_mode((480, 360))
-    pygame.display.set_caption('ピンポンゲームステップ1')
+    pygame.display.set_caption('ピンポンゲーム ステップ5')
 
     # 背景
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((255, 255,255))
 
+    #ボールの処理からパドルと線、呼べるように、グローバル化しておく
     global paddle
     global line
     global score
-    score = 0
-
     global speed
+
+    score = 0
     speed = 6
 
     # ボール初期化
-    #  (初期x座標,初期y座標), (初期角度ラジアン, 速度)
-    ball = Ball((240,100),(-0.6,speed))
+    # (初期x座標,初期y座標), (初期角度ラジアン, 速度)
+    ball = Ball((240,100),(math.pi * 1.75,speed))
 
     # パドル初期化
     paddle = Paddle((10,300))
 
+    # 線の初期化
     line = Line((240,353))
-    
+
+
     # 描く
     ballsprite = pygame.sprite.RenderPlain(ball)
     paddlesprite = pygame.sprite.RenderPlain(paddle)
     linesprite = pygame.sprite.RenderPlain(line)
 
+
     # 画面に出す
     screen.blit(background, (0, 0))
-    #pygame.display.flip()
 
-    # Initialise clock
+    # 動きを管理するClockを初期化
     clock = pygame.time.Clock()
 
+    #フォント指定
     font = pygame.font.SysFont('arial', 15)
+
+    #スコアとスピードを描く    
     scoretext = font.render('Score: {:03d}'.format(score), True, (0,0,0))
     screen.blit(scoretext, (10,5))
     
     speedtext = font.render('Speed:  {:02d}'.format(speed), True, (0,0,0))
     screen.blit(speedtext, (10,28))
+
     
-    # Event loop
+
+    # ずっと処理
     while 1:
-        # Make sure game doesn't run at more than 60 frames per second
+
+        # 一秒に最大60回処理
         clock.tick(60)
 
+        # スコアとスピードを消して(白く塗って)からもう一回描く
         scoretext.fill((255, 255,255))
         screen.blit(scoretext, (10,5))
         scoretext = font.render('Score: {:03d}'.format(score), True, (0,0,0))
@@ -154,13 +211,21 @@ def main():
         screen.blit(speedtext, (10,28))
         speedtext = font.render('Speed:  {:02d}'.format(speed), True, (0,0,0))
         screen.blit(speedtext, (10,28))
-        
+
+
+        #イベント処理
         for event in pygame.event.get():
+
+            #画面を閉じるボタン
             if event.type == QUIT:
                 return
+
+            #キーが押されたとき
             if event.type == KEYDOWN:
-                if event.key == K_q:
+                if event.key == K_q or event.key == K_ESCAPE:
                     return
+                
+                #スピードの上下処理
                 if event.key == K_UP:
                     speed += 1
                     if speed > 99:
@@ -168,19 +233,26 @@ def main():
                 if event.key == K_DOWN:
                     speed -= 1
                     if speed < 1:
-                        speed = 1
+                        speed = 1                    
+                    
 
+        #ボールとパドルがあった場所に背景をもう一回描く
         screen.blit(background, ball.rect, ball.rect)
         screen.blit(background, paddle.rect, paddle.rect)
         screen.blit(background, line.rect, line.rect)
 
+
+        #ボールとパドルのアップデート処理(位置計算)
         ballsprite.update()
         paddlesprite.update()
 
+
+        #ボールを描く
         ballsprite.draw(screen)
         paddlesprite.draw(screen)
         linesprite.draw(screen)
 
+        #表示更新
         pygame.display.flip()
 
 
