45f30843e86f48028ad234a8c3d1cadec95ab7ca
Ken Onodera <onodera_ken@s05.itscom.net>
2023-04-14 09:19:03 +0900

    add Memory.py

---
+++ code/python/Memory.py
@@ -0,0 +1,37 @@
+import random
+import time
+import os
+import copy
+
+input("enterキーを押して下さい")
+
+print("3秒間5つの数字を表示しますので、記憶してください")
+
+numbers = []
+for i in range(5):
+	numbers.append(random.randrange(1, 100))
+
+print(" ".join(list(map(str, numbers))))
+
+time.sleep(3)
+
+os.system('clear')
+
+print("5つの数字をすべて入力してください")
+
+answer = copy.copy(numbers)
+
+while len(answer) > 0:
+	n = int(input())
+	if n in answer:
+		print("正解")
+		answer.remove(n)
+	else:
+		print("不正解")
+		break
+		
+if len(answer) == 0:
+	print("全問正解です")
+else:
+	print("正解は", numbers)
+	
