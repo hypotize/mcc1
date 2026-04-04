d2d9bd5b10ad61dd56184a2d2e25048fdc004092
Ken Onodera <onodera_ken@s05.itscom.net>
2026-04-04 13:11:47 +0900

    fixed bug for puzzle15.py avoid impossible pattern

--- code/python/pyxel/rpg2/puzzle15.py
+++ code/python/pyxel/rpg2/puzzle15.py
@@ -5,96 +5,115 @@ import random
 
 class Puzzle15(game.Game):
 	class Tile:
-		def __init__(self, x, y):
-			self.tile = [[None] * 4 for _ in range(4)]
-			self.x = x
-			self.y = y
-			self.id = y*4+x
-			for i in range(4):
-				for j in range(4):
-					self.tile[i][j] = pyxel.tilemaps[0].pget(x*4 + j, y*4 + i)
+		EMPTY = 15
+		def __init__(self, x=None, y=None):
 			self.isMovable = False
-		def move(self, cb):
-			if self.isMovable:
+			self.tile = [[None] * 4 for _ in range(4)]
+			if x == None and y == None:
+				self.x = 3
+				self.y = 3
+				self.id = Puzzle15.Tile.EMPTY
+				for y in range(4):
+					for x in range(4):
+						self.tile[y][x] = (0, 6)	
+			else:
+				self.x = x
+				self.y = y
+				self.id = y*4+x
 				for i in range(4):
 					for j in range(4):
-						pyxel.tilemaps[0].pset(cb.x*4 + j, cb.y*4 + i, self.tile[i][j])
-						pyxel.tilemaps[0].pset(self.x*4 + j, self.y*4 + i, cb.tile[i][j])
-				self.id, self.tile, cb.id, cb.tile = cb.id, cb.tile, self.id, self.tile
+						self.tile[i][j] = pyxel.tilemaps[0].pget(x*4 + j, y*4 + i)
+		def set(self, x, y):
+			for i in range(4):
+				for j in range(4):
+					pyxel.tilemaps[0].pset(x*4 + j, y*4 + i, self.tile[i][j])
 		def __str__(self):
 			return "({}, {}, {}, {})".format(self.id, self.x, self.y, self.isMovable)
 				
 	class PlayState(state.State):
 		def __init__(self, game):
 			self.game = game
-			self.tiles = [[None] * 4 for _ in range(4)]			
+			self.tiles = [[None] * 4 for _ in range(4)]	
+			self.arr = []	
 			for y in range(4):
 				for x in range(4):
-					self.tiles[y][x] = Puzzle15.Tile(x, y)
-			self.empty = self.tiles[3][3]
-			for y in range(4):
-				for x in range(4):
-					self.empty.tile[y][x] = (0, 6)			
+					self.arr.append(Puzzle15.Tile(x, y))
+			self.save, self.arr[15] = self.arr[15], Puzzle15.Tile()
+			self.empty = None
 			self.start = False
-		def setMovable(self, empty=None):
-			if empty is not None:
-				ex, ey = empty.x, empty.y
-			else:
-				ex, ey = -1, -1
-			self.randlist.clear()
+		def setMovable(self):
 			for y in range(4):
 				for x in range(4):
-					self.tiles[y][x].isMovable = False
-			for y in range(4):
-				for x in range(4):
-					tile = self.tiles[y][x]
-					if tile.id == 15:
-						self.empty = tile
-						if x > 0 and (ex != x-1 or ey != y):
-							self.tiles[y][x-1].isMovable = True
-							self.randlist.append(self.tiles[y][x-1])
-						if x < 3 and (ex != x+1 or ey != y):
-							self.tiles[y][x+1].isMovable = True
-							self.randlist.append(self.tiles[y][x+1])
-						if y > 0 and (ex != x or ey != y-1):
-							self.tiles[y-1][x].isMovable = True
-							self.randlist.append(self.tiles[y-1][x])
-						if y < 3 and (ex != x or ey != y+1):
-							self.tiles[y+1][x].isMovable = True
-							self.randlist.append(self.tiles[y+1][x])
-						break
+					if self.tiles[y][x].id == Puzzle15.Tile.EMPTY:
+						self.tiles[y][x].isMovable = False
+						self.empty = (x, y)
+						continue
+					if y > 0 and self.tiles[y-1][x].id == Puzzle15.Tile.EMPTY:
+						self.tiles[y][x].isMovable = True
+					elif y < 3 and self.tiles[y+1][x].id == Puzzle15.Tile.EMPTY:
+						self.tiles[y][x].isMovable = True
+					elif x > 0 and self.tiles[y][x-1].id == Puzzle15.Tile.EMPTY:
+						self.tiles[y][x].isMovable = True
+					elif x < 3 and self.tiles[y][x+1].id == Puzzle15.Tile.EMPTY:
+						self.tiles[y][x].isMovable = True
+					else:
+						self.tiles[y][x].isMovable = False
 		def checkComplete(self):
 			for y in range(4):
 				for x in range(4):
-					if self.tiles[y][x].id % 4 != x or self.tiles[y][x].id // 4 != y:
+					if self.tiles[y][x].x != x or self.tiles[y][x].y != y:
 						return False
 			return True
+		def countInversions(self):
+			inversions = 0
+			for i in range(16):
+				if self.arr[i].id == Puzzle15.Tile.EMPTY:
+					continue
+				for j in range(i+1, 16):
+					if self.arr[j].id == Puzzle15.Tile.EMPTY:
+						continue
+					if self.arr[i].id > self.arr[j].id:
+						inversions += 1
+			return inversions
+		def isSolvable(self):
+			inversions = self.countInversions()
+			emptyIndex = [i for i, x in enumerate(self.arr) if x.id == Puzzle15.Tile.EMPTY][0]
+			emptyRowFromBottom = 4 - (emptyIndex // 4)
+			if emptyRowFromBottom % 2 == 0:
+				return inversions % 2 == 1
+			else:
+				return inversions % 2 == 0
 		def update(self):
 			if not self.start:
-				self.randlist = []
-				self.setMovable(self.empty)
-				for i in range(50):
-					random.choice(self.randlist).move(self.empty)
-					self.setMovable(self.empty)
+				while True:
+					random.shuffle(self.arr)
+					if self.isSolvable():
+						break
+				for y in range(4):
+					for x in range(4):
+						tile = self.arr.pop(0)
+						self.tiles[y][x] = tile
+						tile.set(x, y)
+				self.setMovable() 
 				self.start = True
 			if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
 				x, y = pyxel.mouse_x // 32, pyxel.mouse_y // 32
-				tile = self.tiles[y][x]
-				tile.move(self.empty)
+				if not self.tiles[y][x].isMovable:
+					return None
+				ex, ey = self.empty
+				self.tiles[ey][ex], self.tiles[y][x] = self.tiles[y][x], self.tiles[ey][ex] 
+				self.tiles[ey][ex].set(ex, ey)
+				self.tiles[y][x].set(x, y)
 				self.setMovable()
 				if self.checkComplete():
 					for y in range(4):
 						for x in range(4):
-							pyxel.tilemaps[0].pset(3*4+x, 3*4+y, self.game.save[y][x])
+							pyxel.tilemaps[0].pset(3*4+x, 3*4+y, self.save.tile[y][x])
 					return state.GameOverState(True, 90, 8)
 			return None
 		def draw(self, xoffset, yoffset):
 			pass
 	def __init__(self):
-		self.save = [[None] * 4 for _ in range(4)]
-		for y in range(4):
-			for x in range(4):
-				self.save[y][x] = pyxel.tilemaps[0].pget(3*4+x, 3*4+y)
 		super().__init__(0, 0, state.TitleState("Puzzle15 Game", state.WaitState(60, Puzzle15.PlayState(self))))
 	def __str__(self):
 		return "Puzzle15"
